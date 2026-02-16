f2f438a74d3410fe88531aec2f23718dc7a8fd2a
victor3spoir <victorespoir.dev@gmail.com>
2026-02-16 16:57:38 +0000

    remove bg  image for now

--- mdvault/features/shared/components/landing-component.tsx
+++ mdvault/features/shared/components/landing-component.tsx
@@ -52,7 +52,7 @@ const LandingComponent = ({
       {/* Enhanced Background Elements */}
       <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-size-[44px_44px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
 
-      <LandingBackground />
+      {/* <LandingBackground /> */}
 
       {/* Animated gradient orbs */}
       <div className="absolute top-0 left-1/2 -z-10 h-100 w-150 -translate-x-1/2 bg-primary/15 blur-[120px] opacity-60 animate-pulse" />
