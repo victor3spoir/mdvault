619a51e999b5179b8f46f82354d11d476e212790
victor3spoir <victorespoir.dev@gmail.com>
2026-02-27 21:30:04 +0000

    chore: finalize media actions & cache hook after debugging

--- mdvault/features/medias/hooks/use-media-cache.ts
+++ mdvault/features/medias/hooks/use-media-cache.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { getMediaDataUrlAction } from "../medias.actions";
 
 interface MediaCacheState {
--- mdvault/features/medias/medias.actions.ts
+++ mdvault/features/medias/medias.actions.ts
@@ -1,9 +1,9 @@
 "use server";
 
-import { cacheTag, updateTag } from "next/cache";
-import { v4 as uuidv4 } from "uuid";
 import { validateImageFile } from "@/lib/file-validation";
 import octokit, { githubRepoInfo } from "@/lib/octokit";
+import { cacheTag, updateTag } from "next/cache";
+import { v4 as uuidv4 } from "uuid";
 import { listArticlesAction } from "../articles/articles.actions";
 import type { ActionResult } from "../shared/shared.types";
 import type { MediaFile, MediaUsage } from "./medias.types";
